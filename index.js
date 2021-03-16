39e137d5aac915bc5319c61c71f21c2adbc9dd68
alexanderyeremi <80661362+alexanderyeremi@users.noreply.github.com>
2021-03-16 20:12:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const vcard = 'BEGIN:VCARD\n'
             + 'VERSION:3.0\n' 
             + 'FN:Affis Admin\n' 
             + 'ORG: Pengembang XBot;\n' 
-            + 'TEL;type=CELL;type=VOICE;waid=6282334297175:+62 823-3429-7175\n' 
+            + 'TEL;type=CELL;type=VOICE;waid=6281281783202:+62 812-8178-3202\n' 
             + 'END:VCARD' 
 blocked = []   
 prefix = '#'
@@ -47,7 +47,7 @@ cr = '*BOT INI SUDAH TERVERIFIKASI*'
 /*************************************/
 
 /******** OWNER NUMBER**********/
-const ownerNumber = ["62895710073737@s.whatsapp.net","6282334297175@s.whatsapp.net"] 
+const ownerNumber = ["6281281783202@s.whatsapp.net","6281281783202@s.whatsapp.net"] 
 /************************************/
 
        
